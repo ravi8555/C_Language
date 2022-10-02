db05363a2e542b5b9138d4ef94b9817b290cf4a2
Ravindra Dhadave <ravi8555@gmail.com>
2022-10-02 19:38:46 +0530

    inital

---
+++ c_language/first.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+// #define PI 3.14159
+// void main()
+// {
+//     // printf("Hello");
+
+//     // int num = 1000;
+//     // float num2 = 10.2;
+//     // char num3 = 'A';
+
+//     // // printf("%d %c %f", num3, num, num2);
+
+//     // // printf("%c", num3);
+//     // printf("The value of integer num is  %d \nThe value of float is %f \nThe value of Char is %c", num, num2, num3);
+
+//     // Sacanf
+
+//     // int age;
+//     // printf("Enter your Age");
+
+//     // scanf("%d", &age);
+//     // printf("Your Age Is :%d", age);
+
+//     // int num1, num2;
+//     // char Lett;
+
+//     // printf("Enter Two Numbers and char:");
+//     // scanf("%d %d %c", &num1, &num2, &Lett);
+
+//     // printf("The value of num1 is %d and Value of nu2 is %d and Charector %c", num1, num2, Lett);
+
+//     // const float PI = 3.14159;
+//     // #define PI 3.14159 above vioid:main function
+
+//     float r, area, parameter;
+//     printf("Enter the radius value: ");
+//     scanf("%f", &r);
+
+//     area = PI * r * r;
+//     parameter = 2 * PI * r;
+//     printf("Area of Circle is %f \n", area);
+//     printf("Area of parameter is %f \n", parameter);
+// }
+
+// #define print printf
+
+void main()
+{
+    // print("Hello");
+    // float math;
+    // float science;
+
+    // typedef float marks;
+    // marks math;
+    // marks science;
+
+    // math = 87.7;
+    // science = 78;
+
+    // printf("Marks in math is : %f /n Marks in science is : %f", math, science);
+
+    int a = 10;
+    int b = 3;
+    // int c = 0;
+
+    // c = a + b;
+
+    float c = 0;
+    c = a / b;
+
+    printf("Value c is : %f", c);
+}
